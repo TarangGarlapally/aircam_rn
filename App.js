1fc9037d30befcd8f8c1a2585fc0ee055ebcf339
TarangGarlapally <taranggarlapally@gmail.com>
2021-02-21 00:16:03 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,157 @@
+import React from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+  StatusBar,
+  Dimensions,
+  TouchableOpacity,
+  PermissionsAndroid,
+} from 'react-native';
+
+
+import { NodeCameraView } from "react-native-nodemediaclient";
+
+const styles = StyleSheet.create({
+  view: {
+    height: Dimensions.get('window').height,
+    width: Dimensions.get('window').width,
+    position: 'relative',
+  },
+  buttonWrapper: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: Dimensions.get('window').width,
+    height: 50,
+    position: 'absolute',
+    zIndex: 2,
+    bottom: 50,
+  },
+  button: {
+    width: 200,
+    height: 40,
+    backgroundColor: '#014484',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingLeft: 15,
+    paddingRight: 15,
+  },
+  buttonText: {
+    color: '#ffffff',
+    fontFamily: 'system',
+  },
+});
+
+const config = {
+  cameraConfig: {
+    cameraId: 1,
+    cameraFrontMirror: false
+  },
+  videoConfig: {
+    preset: 4,
+    bitrate: 2000000,
+    profile: 2,
+    fps: 30,
+    videoFrontMirror: true,
+  },
+  audioConfig: {
+    bitrate: 128000,
+    profile: 1,
+    samplerate: 44100,
+  }
+};
+class App extends React.Component {
+  vb = null;
+
+
+  state = {
+    isStreaming: false,
+  };
+
+
+  videoSettings = {
+    preset: 12,
+    bitrate: 400000,
+    profile: 1,
+    fps: 15,
+    videoFrontMirror: false,
+  };
+
+
+  cameraSettings = {cameraId: 1, cameraFrontMirror: true};
+
+
+  audioSettings = {bitrate: 32000, profile: 1, samplerate: 44100};
+
+
+  channel = 'nodeskwela';
+
+
+  get height() {
+    return Dimensions.get('window').height;
+  }
+
+
+  get width() {
+    return Dimensions.get('window').width;
+  }
+
+
+  toggleStream = async () => {
+    await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.CAMERA);
+
+
+    if (this.state.isStreaming) {
+      this.vb.stop();
+    } else {
+      this.vb.start();
+    }
+    this.setState({
+      isStreaming: !this.state.isStreaming,
+    });
+  };
+
+
+  render() {
+    const streamKey = 'abc';
+  const url = `rtmp://192.168.0.3/stream/${streamKey}`;
+
+    return (
+      <>
+        <StatusBar barStyle="dark-content" />
+        <View style={styles.view}>
+          <NodeCameraView
+            style={{
+              height: this.height,
+              width: this.width,
+              zIndex: 1,
+              backgroundColor: '#000000',
+            }}
+            ref={vb => {
+              this.vb = vb;
+            }}
+            outputUrl={url}
+           camera={config.cameraConfig}
+        audio={config.audioConfig}
+        video={config.videoConfig}
+            autopreview={true}></NodeCameraView>
+          <View style={styles.buttonWrapper}>
+            <TouchableOpacity onPress={this.toggleStream}>
+              <View style={styles.button}>
+                <Text style={styles.buttonText}>
+                  {this.state.isStreaming
+                    ? 'Stop Streaming'
+                    : 'Start Streaming'}
+                </Text>
+              </View>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </>
+    );
+  }
+}
+
+
+export default App;
+
